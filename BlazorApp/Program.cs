e551292ab915068b6a672072f827a59c47ee7c00
Luckerok882 <denishronenko@gmail.com>
2025-11-06 01:12:31 +0200

    register

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -3,10 +3,18 @@ using BlazorApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddHttpClient("Api", client =>
+{
+    // Указываем базовый адрес твоего бэкенда (из Swagger)
+    client.BaseAddress = new Uri("http://localhost:5143"); 
+});
+
+
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
