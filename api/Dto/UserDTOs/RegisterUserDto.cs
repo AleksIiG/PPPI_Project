e9a525ec84c72f36502f4a4a9754c0f8500632b5
AleksIiG <kyzmin21102006@gmail.com>
2025-11-05 18:30:55 +0200

    return

--- api/Dto/UserDTOs/RegisterUserDto.cs
+++ api/Dto/UserDTOs/RegisterUserDto.cs
@@ -8,6 +8,7 @@ namespace api.Dto.UserDTOs
 {
     public class RegisterUserDto
     {
+        public string Id { get; set; } = string.Empty;
         [Required]
         [StringLength(50, MinimumLength = 3)]
         public string Username { get; set; } = string.Empty;
